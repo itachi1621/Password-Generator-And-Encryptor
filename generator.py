eb829d547bac38463b03d5818d46658e233b8e13
Zero <zero0396@gmail.com>
2023-03-01 01:31:51 -0400

    first

--- generator.py
+++ generator.py
@@ -1,5 +1,14 @@
 from custom_functions.menus import *;
+import argparse;
 
 #displayPasswordList(passwordList);
 #print(passwordList);
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--password", help="Password to encrypt")
+#Encrytion method for sha
+args = parser.parse_args()
+parser.parse_args()
+
+
+
 mainMenu();
